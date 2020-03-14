b6967e17bcdb3711240b4e07e1f7fff3a0d46640
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-14 14:16:56 +0700

    Data types var & val can String, Int, Float, Double etc

---
+++ src/DataTypes.kt
@@ -0,0 +1,23 @@
+/*
+* Created by lukman on 14/03/20 / 14.01
+* Project Name : KotlinBasic
+*/
+fun main(args: Array<String>) {
+
+
+    var name:String="dede"
+    var age:Int=34
+    var dep:String?
+    dep="Software Engineering"
+    println("name:"+name)
+    println("age:"+age)
+    println("department"+dep)
+    var pi:Double=3.4
+    val name2="lukman" // val is variable canot change
+//    name2="dede" this is can error
+
+
+
+
+
+}
