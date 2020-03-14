e4c5d7745f09f539fe67f85d240750182f0a58c9
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-14 13:39:49 +0700

    Initial commit

---
+++ src/FirstApp.kt
@@ -0,0 +1,13 @@
+/*
+* Created by lukman on 14/03/20 / 13.20
+* Project Name : KotlinBasic
+*/
+
+fun main(args:Array<String>){
+
+    print("hi Lukman,\n")
+    println("Welcome to Kotlin")
+    print(150)
+
+}
+
