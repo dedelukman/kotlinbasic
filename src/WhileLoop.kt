0ccd89fbf6c18ea2e4cbf862b2e504545b78d66a
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-15 14:19:51 +0700

    while loop condition

---
+++ src/WhileLoop.kt
@@ -0,0 +1,16 @@
+/*
+* Created by lukman on 15/03/20 / 14.16
+* Project Name : KotlinBasic
+*/
+fun main(args: Array<String>) {
+    var count=1
+    while (count <=5)
+    { // block of code
+        if (count==2)
+            println("count:$count")
+        count++
+    }
+
+    println("loop done")
+
+}
