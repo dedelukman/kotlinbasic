f1c727763a967e441d62eaf6b195d44311ab73de
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-16 03:16:56 +0700

    Function Polymorphism basic

---
+++ src/FunctionPolymorphism.kt
@@ -0,0 +1,26 @@
+/*
+* Created by lukman on 16/03/20 / 03.10
+* Project Name : KotlinBasic
+*/
+
+fun show(name:String):Unit{
+    print("name:$name")
+
+}
+
+fun show(number:Int):Unit{
+    print("number:$number")
+
+}
+
+fun show(numberD: Double):Unit{
+    print("numberD:$numberD")
+
+}
+fun main(args: Array<String>) {
+
+    show("dede")
+    show(10.3)
+    show(299)
+
+}
