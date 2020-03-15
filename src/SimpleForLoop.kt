ab667186e8ee52dc8a68f19b750b1431e3fcd4d9
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-15 14:09:10 +0700

    for loop condition

---
+++ src/SimpleForLoop.kt
@@ -0,0 +1,14 @@
+/*
+* Created by lukman on 15/03/20 / 14.02
+* Project Name : KotlinBasic
+*/
+fun main(args: Array<String>) {
+
+    for (item in 1..10)
+    { // block of code
+        if (item==2)
+        println("count:$item")
+    }
+
+    println("loop done")
+}
