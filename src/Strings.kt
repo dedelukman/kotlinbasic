45740ecce4f89d2f710f9111d936ac16309b80e2
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-16 01:48:15 +0700

    basic manipulated strings

---
+++ src/Strings.kt
@@ -0,0 +1,19 @@
+/*
+* Created by lukman on 16/03/20 / 01.37
+* Project Name : KotlinBasic
+*/
+fun main(args: Array<String>) {
+
+    var title=" welcome to jakarta?"
+    println(title)
+    println("title:"+title)
+    println("title: $title")
+    var name="lukman " + "hakim"
+    println("name: $name")
+    println("Second Char: "+title[1])
+    println("title:"+ title.toUpperCase())
+    println("title:"+title.toLowerCase())
+    println("title:"+title.split(" "))
+    println("title:"+title.trim())
+
+}
