8fae0e2fbcc6d15261728b8dc117edfe62306c81
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-16 03:21:01 +0700

    global and local variable

---
+++ src/Scope.kt
@@ -0,0 +1,15 @@
+/*
+* Created by lukman on 16/03/20 / 03.17
+* Project Name : KotlinBasic
+*/
+var fullname="dede lukmanul hakim" // global variable
+fun display():Unit{
+    println(fullname)
+}
+fun main(args: Array<String>) {
+
+    var name="dede"
+    println(fullname)
+    print("name:$name")
+
+}
