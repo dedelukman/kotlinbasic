d15d4abc33accf6f1d3db8e5f13865db7ff0eecf
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-15 13:02:53 +0700

    logic condition

---
+++ src/LogicCondition.kt
@@ -0,0 +1,14 @@
+/*
+* Created by lukman on 15/03/20 / 00.55
+* Project Name : KotlinBasic
+*/
+fun main(args: Array<String>) {
+    var n:Int= 9
+    println(n>=0 && n<10)
+
+    var n2:Int= 55
+    println(n==2 || n>100)
+
+    var IsMarried:Boolean=false
+    print(!IsMarried)
+}
