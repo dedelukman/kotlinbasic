9cc3515bbdf36d5d54ee3aa8686dfd153e709f97
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-21 03:02:51 +0700

    how to use extensions, like make swap

---
+++ src/Extensions.kt
@@ -0,0 +1,19 @@
+/*
+* Created by lukman on 21/03/20 / 02.50
+* Project Name : KotlinBasic
+*/
+fun ArrayList<String>.swap(index1:Int, index2:Int){
+    var temp=this.get(index1)
+    this.set(index1, this.get(index2))
+    this.set(index2,temp)
+}
+fun main(args: Array<String>) {
+    var arrayList=ArrayList<String>()
+    arrayList.add("maryam")
+    arrayList.add("khodijah")
+    arrayList.add("hafshah")
+    arrayList.add("zainab")
+    arrayList.swap(0,1)
+    println(arrayList)
+
+}
