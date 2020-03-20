d9f71964c28c39635c555ad3763900b4585c41bd
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-21 03:19:10 +0700

    how to use inner and outer class

---
+++ src/NestedClass.kt
@@ -0,0 +1,20 @@
+/*
+* Created by lukman on 21/03/20 / 03.15
+* Project Name : KotlinBasic
+*/
+
+class Outer{
+    private val name:String?=null
+
+    inner class Nested{
+        fun Show(){
+            println(name)
+        }
+    }
+}
+fun main(args: Array<String>) {
+    var outer=Outer()
+    var nested=Outer.Nested()
+    nested.Show()
+
+}
