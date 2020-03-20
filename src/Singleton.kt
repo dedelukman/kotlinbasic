a0de0b076908962168c564d130743ccb01a3f0e6
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-21 03:32:05 +0700

    how to use lazy and make singleton design pattern

---
+++ src/Singleton.kt
@@ -0,0 +1,27 @@
+/*
+* Created by lukman on 21/03/20 / 03.20
+* Project Name : KotlinBasic
+*/
+
+class  Singleton{
+    var name:String?=null
+    private constructor(){
+        println("instance is created")
+    }
+
+    companion object{
+        val instance:Singleton by lazy {Singleton()}
+    }
+}
+fun main(args: Array<String>) {
+
+    var s1=Singleton.instance
+    s1.name="lukman"
+    println(s1.name)
+
+    var s2=Singleton.instance
+    println(s2.name)
+
+    var s3=Singleton.instance
+    println(s2.name)
+}
