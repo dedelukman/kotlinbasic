0d05fe0aa1f11c93015bff9000e4d8983f2143c0
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-21 03:15:10 +0700

    how to use package

---
+++ src/CallPackage.kt
@@ -0,0 +1,12 @@
+import com.example.cars.MyCar
+
+/*
+* Created by lukman on 21/03/20 / 03.07
+* Project Name : KotlinBasic
+*/
+fun main(args: Array<String>) {
+
+    var myCar = MyCar()
+   myCar.ShowInfo()
+
+}
---
+++ src/com/example/cars/MainClass.kt
@@ -0,0 +1,16 @@
+package com.example.cars
+
+/*
+* Created by lukman on 21/03/20 / 03.09
+* Project Name : KotlinBasic
+*/
+class MyCar{
+    fun ShowInfo(){
+        println("BMW is new car ")
+    }
+}
+
+fun main(args: Array<String>) {
+
+
+}
