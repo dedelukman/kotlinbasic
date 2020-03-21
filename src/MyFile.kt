2b68d611e942314eb2c9741f7e346935bc702bbb
Dede Lukmanul Hakim <dlukmanul.h@gmail.com>
2020-03-21 07:48:26 +0700

    basic file read

--- src/MyFile.kt
+++ src/MyFile.kt
@@ -1,3 +1,4 @@
+import java.io.FileReader
 import java.io.FileWriter
 
 /*
@@ -5,16 +6,39 @@ import java.io.FileWriter
 * Project Name : KotlinBasic
 */
 fun main(args: Array<String>) {
-    print("Write to file text : ")
-    var str:String= readLine().toString()
-    WriteToFile(str)
+    println("1- read\n2- write \n")
+    val op= readLine()!!.toInt()
+    when(op){
+        1-> ReadfromFile()
+        2->{
+            print("Write to file text : ")
+            var str:String= readLine().toString()
+            WriteToFile(str)
+        }
+    }
+
+
 }
 
 fun WriteToFile(str:String){
     try {
-    var fo=FileWriter("text.txt",true)
-    fo.write(str+"\n")
-    fo.close()
+        val fo=FileWriter("text.txt",true)
+        fo.write(str+"\n")
+        fo.close()
+        println("data is saved")
+    }catch (ex:Exception){
+        print(ex.message)
+    }
+}
+
+fun ReadfromFile(){
+    try {
+        var fin=FileReader("text.txt")
+        var c:Int?
+        do {
+            c=fin.read()
+            print(c.toChar())
+        }while (c!=-1)
     }catch (ex:Exception){
         print(ex.message)
     }
